2c5ed811b098154fc3a4667858a36388c0892fec
Shepherd <18360863101@126.com>
2017-05-20 00:05:27 +0800

    Create new window

---
+++ src/Win.java
@@ -0,0 +1,21 @@
+import javax.swing.*;
+import java.awt.*;
+public class Win {
+	public static void main(String args[]){	
+		Dimension screensize = Toolkit.getDefaultToolkit().getScreenSize();
+		int width = (int)screensize.getWidth();
+		int height = (int)screensize.getHeight();
+		JFrame win = new JFrame("MonitoringSystem");
+		Container con = win.getContentPane();
+		con.setBackground(Color.white);
+		win.setBounds(width/4, height/4, width/2, height/2);
+		win.setVisible(true);
+		win.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+		TrayDemo testwin = new TrayDemo();
+		}
+	void miniTray(){
+		PopupMenu pop = new PopupMenu();
+		MenuItem show = new MenuItem("»¹Ô­");
+		MenuItem exit = new MenuItem("ÍË³ö");
+	}
+}
